e0c40ac6833c5e6a08d9ff6726623b12ac610f15
Truls Stenrud <truls.stenrud@gmail.com>
2016-04-05 10:57:57 +0200

    Testing for custom rule

---
+++ GoL/src/test/CustomRuleTest.java
@@ -0,0 +1,62 @@
+package test;
+
+import model.rules.ClassicRule;
+import model.rules.CustomRule;
+import model.rules.Rule2D;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import tools.Utilities;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Truls on 05/04/16.
+ */
+public class CustomRuleTest {    private Rule2D rule;
+
+    @Before
+    public void setUp() throws Exception {
+
+        // Creates a mock grid to be tested
+        boolean[][] grid = new boolean[][]{
+                new boolean[]{false, false, false, false, false},
+                new boolean[]{false, false, true, false, false},
+                new boolean[]{false, false, true, false, false},
+                new boolean[]{false, false, true, false, false},
+                new boolean[]{false, false, false, false, false}};
+
+        // Creates a neighbour count array manually
+        byte[][] neighbours = new byte[][]{
+                new byte[]{0, 1, 1, 1, 0},
+                new byte[]{0, 2, 1, 2, 0},
+                new byte[]{0, 3, 2, 3, 0},
+                new byte[]{0, 2, 1, 2, 0},
+                new byte[]{0, 1, 1, 1, 0}};
+
+        // Constructs classicRule
+        rule = new CustomRule(grid, neighbours, "b3/s23");
+    }
+
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testEvolve() throws Exception {
+
+        rule.evolve();
+
+        Utilities.print2DArray(rule.getGrid());
+
+        assertArrayEquals(new boolean[][]{
+                new boolean[]{false, false, false, false, false},
+                new boolean[]{false, false, false, false, false},
+                new boolean[]{false, true, true, true, false},
+                new boolean[]{false, false, false, false, false},
+                new boolean[]{false, false, false, false, false}}, rule.getGrid());
+    }
+
+}
