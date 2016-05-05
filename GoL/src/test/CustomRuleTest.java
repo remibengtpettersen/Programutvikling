93ed0b7f78051b72a495706a3ae9f27bae8b745a
Andoph <andreas_bo@live.com>
2016-05-05 18:29:25 +0200

    Added RuleParserTest

--- GoL/src/test/CustomRuleTest.java
+++ GoL/src/test/CustomRuleTest.java
@@ -1,15 +1,25 @@
 package test;
 
+import model.DynamicGameOfLife;
 import model.EvolveException;
+import model.GameOfLife;
 import model.rules.CustomRule;
 import model.rules.Rule;
 import org.junit.Before;
 import org.junit.Test;
 import tools.Utilities;
 
+import static org.junit.Assert.assertTrue;
+
 public class CustomRuleTest {
 
+    private DynamicGameOfLife gol;
     private CustomRule rule;
+
+    @Test
+    public void testParrot() {
+        assertTrue(true);
+    }
 
     /**
      * Creates a grid to be evolved and a neighbour grid that is pre populated with neighbours count. The neighbour aggregator is tested elsewhere.
@@ -18,7 +28,7 @@ public class CustomRuleTest {
     public void setUp() throws Exception {
 
         // Creates a mock grid to be tested
-        boolean[][] grid = new boolean[][]{
+        /*boolean[][] grid = new boolean[][]{
                 new boolean[]{false, false, false, false, false},
                 new boolean[]{false, false, true, false, false},
                 new boolean[]{false, false, true, false, false},
@@ -31,7 +41,7 @@ public class CustomRuleTest {
                 new byte[]{0, 2, 1, 2, 0},
                 new byte[]{0, 3, 2, 3, 0},
                 new byte[]{0, 2, 1, 2, 0},
-                new byte[]{0, 1, 1, 1, 0}};
+                new byte[]{0, 1, 1, 1, 0}};*/
 
         // Constructs classicRule
         //rule = new CustomRule(grid, neighbours, "b3/s23");
@@ -54,5 +64,6 @@ public class CustomRuleTest {
                 new boolean[]{false, false, false, false, false},
                 new boolean[]{false, false, false, false, false}}, rule.getGrid());*/
     }
+
 
 }
